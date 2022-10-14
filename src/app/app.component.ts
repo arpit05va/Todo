d8d18e07cf9f9b3923c17e86b6a155b81bf5e63b
arpit05va <arpitvashistha928@gmail.com>
2022-10-14 22:46:51 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  list: any[] = [];
+  addTask(item: string) {
+    this.list.push({ id: this.list.length, name: item });
+
+  }
+  removeTask(id:number) {
+    this.list=this.list.filter(item=>item.id!==id)
+  }
 }
